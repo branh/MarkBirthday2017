f4d44bc16b4d0e38274087a87713e63db9adcd1d
Bran Hagger <bran.hagger@outlook.com>
2017-01-08 13:44:43 -0800

    Initial commit

---
+++ Instructions.js
@@ -0,0 +1,8 @@
+function DisplayInstructions() {
+   var canvas = document.getElementById("IntroCanvas");
+   var context = canvas.getContext("2d");
+   context.fillStyle = "#A52A2A";
+   context.fillRect(0, 0, 500, 500);
+}
+
+DisplayInstructions();
