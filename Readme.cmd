e51f4b51eabb213e69948a30beb8fb5720f14e8c
Bran Hagger <bran.hagger@outlook.com>
2017-01-08 13:54:08 -0800

    Add readme file.

---
+++ Readme.cmd
@@ -0,0 +1,3 @@
+Are you looking for [the actual game](http://branh.github.io/MarkBirthday2017)?
+
+Then go on and [adventure](http://branh.github.io/MarkBirthday2017). You'll need to enable Javascript for this game to work.
