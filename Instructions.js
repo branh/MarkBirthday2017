4c31242bb3d61a05c09698f7ca62e79f987e8afd
Bran Hagger <bran.hagger@outlook.com>
2017-01-09 21:30:42 -0800

    Some simple text writing code.

--- Instructions.js
+++ Instructions.js
@@ -1,8 +1,20 @@
+function DisplayText(context, text, xPos, yPos) {
+	context.fillStyle = "#000000";
+	context.font = "30px Helvetica";
+	context.fillText(text, xPos, yPos);
+}
+
 function DisplayInstructions() {
    var canvas = document.getElementById("IntroCanvas");
    var context = canvas.getContext("2d");
    context.fillStyle = "#A52A2A";
-   context.fillRect(0, 0, 500, 500);
+   context.fillRect(0, 0, 750, 400);
+   
+   DisplayText(context, "Press", 40, 100);
+   context.strokeRect(130, 80, 30, 30);
+   context.moveTo(145, 120);
+   context.lineTo(145, 85);
+   context.stroke();
 }
 
-DisplayInstructions();
+document.addEventListener("DOMContentLoaded", DisplayInstructions, false);
