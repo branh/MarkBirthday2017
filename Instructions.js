905ac4f329eea636cb48a74c3ead6667d86e8f73
Bran Hagger <bran.hagger@outlook.com>
2017-01-10 20:13:18 -0800

    Intro text.

--- Instructions.js
+++ Instructions.js
@@ -4,17 +4,46 @@ function DisplayText(context, text, xPos, yPos) {
 	context.fillText(text, xPos, yPos);
 }
 
+function DisplayUpArrow(context, xPos, yPos) {
+	context.strokeRect(xPos, yPos, 30, 30);
+	
+	context.moveTo(xPos + 15, yPos + 25);
+	context.lineTo(xPos + 15, yPos + 5);
+	context.stroke();
+	
+	context.moveTo(xPos + 5, yPos + 15);
+	context.lineTo(xPos + 15, yPos + 5);
+	context.lineTo(xPos + 25, yPos + 15);
+	context.stroke();
+}
+
+function DisplayDownArrow(context, xPos, yPos) {
+	context.strokeRect(xPos, yPos, 30, 30);
+	
+	context.moveTo(xPos + 15, yPos + 25);
+	context.lineTo(xPos + 15, yPos + 5);
+	context.stroke();
+	
+	context.moveTo(xPos + 5, yPos + 15);
+	context.lineTo(xPos + 15, yPos + 25);
+	context.lineTo(xPos + 25, yPos + 15);
+	context.stroke();
+}
+
 function DisplayInstructions() {
    var canvas = document.getElementById("IntroCanvas");
    var context = canvas.getContext("2d");
-   context.fillStyle = "#A52A2A";
+   context.fillStyle = "#A52A2A"; 
    context.fillRect(0, 0, 750, 400);
    
-   DisplayText(context, "Press", 40, 100);
-   context.strokeRect(130, 80, 30, 30);
-   context.moveTo(145, 120);
-   context.lineTo(145, 85);
-   context.stroke();
+   DisplayText(context, "Defend the farm! Destroy the bandits! Cook!", 40, 100);
+   DisplayText(context, "Press", 40, 170);
+   DisplayUpArrow(context, 130, 140);
+   DisplayText(context, "or", 170, 170);
+   DisplayDownArrow(context, 210, 140);
+   DisplayText(context, "to move.", 260, 170);
+   
+   DisplayText(context, "Press the space bar to shoot.", 40, 230);
 }
 
 document.addEventListener("DOMContentLoaded", DisplayInstructions, false);
